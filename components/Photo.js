601d1a69ba6f4b5b5bb5e8f7c903dcea245e7697
soohyun <soohyun_kim2@tmax.co.kr>
2021-11-01 18:05:31 +0900

    added pull to refresh

--- components/Photo.js
+++ components/Photo.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
 import { Image, TouchableOpacity, useWindowDimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
+import { Ionicons } from '@expo/vector-icons';
 
 const Container = styled.View``;
 const Header = styled.TouchableOpacity`
@@ -14,21 +15,34 @@ const UserAvatar = styled.Image`
   margin-right: 10px;
   width: 25px;
   height: 25px;
-  border-radius: 12.5;
+  border-radius: 12.5px;
 `;
 const Username = styled.Text`
   color: white;
   font-weight: 600;
 `;
 const File = styled.Image``;
-const Actions = styled.View``;
-const Action = styled.TouchableOpacity``;
-const Caption = styled.View``;
+const Actions = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const Action = styled.TouchableOpacity`
+  margin-right: 10px;
+`;
+const Caption = styled.View`
+  flex-direction: row;
+`;
 const CaptionText = styled.Text`
   color: white;
+  margin-left: 10px;
 `;
 const Likes = styled.Text`
   color: white;
+  margin: 7px 0px;
+  font-weight: 600;
+`;
+const ExtraContainer = styled.View`
+  padding: 10px;
 `;
 
 const Photo = ({ id, user, caption, file, isLiked, likes }) => {
@@ -51,17 +65,29 @@ const Photo = ({ id, user, caption, file, isLiked, likes }) => {
         style={{ width, height: imageHeight }}
         source={{ uri: file }}
       />
-      <Actions>
-        <Action />
-        <Action />
-      </Actions>
-      <Likes>{likes === 1 ? '1 like' : `${likes} likes`}</Likes>
-      <Caption>
-        <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
-          <Username>{user.username}</Username>
+      <ExtraContainer>
+        <Actions>
+          <Action>
+            <Ionicons
+              name={isLiked ? 'heart' : 'heart-outline'}
+              color={isLiked ? 'tomato' : 'white'}
+              size={22}
+            />
+          </Action>
+          <Action onPress={() => navigation.navigate('Comments')}>
+            <Ionicons name="chatbubble-outline" color="white" size={22} />
+          </Action>
+        </Actions>
+        <TouchableOpacity onPress={() => navigation.navigate('Likes')}>
+          <Likes>{likes === 1 ? '1 like' : `${likes} likes`}</Likes>
         </TouchableOpacity>
-        <CaptionText>{caption}</CaptionText>
-      </Caption>
+        <Caption>
+          <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+            <Username>{user.username}</Username>
+          </TouchableOpacity>
+          <CaptionText>{caption}</CaptionText>
+        </Caption>
+      </ExtraContainer>
     </Container>
   );
 };
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@apollo/client';
-import React from 'react';
+import React, { useState } from 'react';
 import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import { PHOTO_FRAGMENT, COMMENT_FRAGMENT } from '../fragments';
 import { gql } from '@apollo/client';
@@ -27,16 +27,25 @@ const FEED_QUERY = gql`
 `;
 
 const Feed = () => {
-  const { data, loading } = useQuery(FEED_QUERY);
+  const { data, loading, refetch } = useQuery(FEED_QUERY);
+  const [refreshing, setRefreshing] = useState(false);
 
   const renderPhoto = ({ item: photo }) => {
     console.log(photo);
     return <Photo {...photo} />;
   };
 
+  const refresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
   return (
     <ScreenLayout loading={loading}>
       <FlatList
+        refreshing={refreshing}
+        onRefresh={refetch}
         style={{ width: '100%' }}
         showsVerticalScrollIndicator={false}
         data={data?.seeFeed}
