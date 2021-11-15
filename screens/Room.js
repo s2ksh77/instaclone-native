c444b452f491039d04a8d7205743c8838de76ab0
soohyun <soohyun_kim2@tmax.co.kr>
2021-11-15 18:36:15 +0900

    message

--- screens/Room.js
+++ screens/Room.js
@@ -1,6 +1,17 @@
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, KeyboardAvoidingView } from 'react-native';
 import { gql, useQuery } from '@apollo/client';
+import ScreenLayout from '../components/ScreenLayout';
+import styled from 'styled-components/native';
+
+const SEND_MESSAGE_MUTATION = gql`
+  mutation sendMessage($payload: String!, $roomId: Int!, $userId: Int!) {
+    sendMessage(payload: $payload, roomId: $roomId, userId: $userId) {
+      ok
+      id
+    }
+  }
+`;
 
 const ROOM_QUERY = gql`
   query seeRoom($id: Int!) {
@@ -18,8 +29,39 @@ const ROOM_QUERY = gql`
   }
 `;
 
+const MessageContainer = styled.View`
+  padding: 0px 10px;
+  flex-direction: ${(props) => (!props.outGoing ? 'row-reverse' : 'row')};
+  align-items: flex-end;
+`;
+const Author = styled.View``;
+const Avatar = styled.Image`
+  width: 20px;
+  height: 20px;
+  border-radius: 25px;
+`;
+
+const Message = styled.Text`
+  color: white;
+  background-color: rgba(255, 255, 255, 0.3);
+  padding: 5px 10px;
+  border-radius: 10px;
+  overflow: hidden;
+  font-size: 16px;
+  margin: 0px 10px;
+`;
+
+const TextInput = styled.TextInput`
+  margin: 50px 0px 10px 0px;
+  width: 95%;
+  border: 1px solid rgba(255, 255, 255, 0.5);
+  padding: 10px 20px;
+  border-radius: 1000px;
+  color: white;
+`;
+
 const Room = ({ route, navigation }) => {
-  const { data } = useQuery(ROOM_QUERY, {
+  const { data, loading } = useQuery(ROOM_QUERY, {
     variables: {
       id: route?.params?.id,
     },
@@ -31,10 +73,40 @@ const Room = ({ route, navigation }) => {
       title: `Conversation with ${route?.params?.talkingTo?.username}`,
     });
   }, []);
+
+  const renderItem = ({ item: message }) => (
+    <MessageContainer
+      outGoing={message.user.username === route?.params?.talkingTo?.username}
+    >
+      <Author>
+        <Avatar source={{ uri: message.user.avatar }} />
+      </Author>
+      <Message>{message.payload}</Message>
+    </MessageContainer>
+  );
+
   return (
-    <View>
-      <Text>Message List</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: 'black' }}
+      //   behavior="height"
+      //   keyboardVerticalOffset={50}
+    >
+      <ScreenLayout loading={loading}>
+        <FlatList
+          style={{ width: '100%' }}
+          data={data?.seeRoom?.messages}
+          keyExtractor={(message) => '' + message.id}
+          renderItem={renderItem}
+          inverted
+        />
+        <TextInput
+          placeholder="Write a message..."
+          placeholderTextColor="rgba(255, 255, 255, 0.5)"
+          returnKeyLabel="전송"
+          returnKeyType="send"
+        />
+      </ScreenLayout>
+    </KeyboardAvoidingView>
   );
 };
 
