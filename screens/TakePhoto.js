a5029b925e2cd31331d9107945bb982b011d0d0a
soohyun <soohyun_kim2@tmax.co.kr>
2021-11-15 12:23:47 +0900

    take a photo

--- screens/TakePhoto.js
+++ screens/TakePhoto.js
@@ -1,11 +1,189 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, TouchableOpacity, StatusBar, Image } from 'react-native';
+import styled from 'styled-components/native';
+import { Camera } from 'expo-camera';
+import { Ionicons } from '@expo/vector-icons';
+import Slider from '@react-native-community/slider';
+import * as MediaLibrary from 'expo-media-library';
 
-const TakePhoto = () => {
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Actions = styled.View`
+  flex: 0.35;
+  padding: 0px 50px;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const TakePhotoBtn = styled.TouchableOpacity`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border: 2px solid rgba(255, 255, 255, 0.8);
+  border-radius: 50px;
+`;
+
+const SliderContainer = styled.View``;
+
+const ButtonsContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ActionsContainer = styled.View`
+  flex-direction: row;
+`;
+
+const CloseButton = styled.TouchableOpacity`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+`;
+
+const PhotoAction = styled.TouchableOpacity`
+  background-color: white;
+  padding: 5px 10px;
+  border-radius: 4px;
+`;
+const PhotoActionText = styled.Text`
+  font-weight: 600;
+`;
+
+const TakePhoto = ({ navigation }) => {
+  const [ok, setOk] = useState(false);
+  const camera = useRef();
+  const [cameraReady, setCameraReady] = useState(false);
+  const [takenPhoto, setTakenPhoto] = useState('');
+  const [zoom, setZoom] = useState(0);
+  const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const getPermissions = async () => {
+    const { granted } = await Camera.requestCameraPermissionsAsync();
+    setOk(permissions);
+  };
+
+  useEffect(() => {
+    getPermissions();
+  }, []);
+
+  const onCameraSwitch = () => {
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else {
+      setCameraType(Camera.Constants.Type.front);
+    }
+  };
+
+  const onZoomValueChange = (e) => {
+    setZoom(e);
+  };
+  const onFlashChange = () => {
+    if (flashMode === Camera.Constants.FlashMode.off) {
+      setFlashMode(Camera.Constants.FlashMode.on);
+    } else if (flashMode === Camera.Constants.FlashMode.on) {
+      setFlashMode(Camera.Constants.FlashMode.auto);
+    } else if (flashMode === Camera.Constants.FlashMode.auto) {
+      setFlashMode(Camera.Constants.FlashMode.off);
+    }
+  };
+
+  const onCameraReady = () => setCameraReady(true);
+
+  const takePhoto = async () => {
+    if (camera.current && cameraReady) {
+      const { uri } = await camera.current.takePictureAsync({
+        quality: 1,
+        exif: true,
+      });
+      setTakenPhoto(uri);
+      // const asset = await MediaLibrary.createAssetAsync(uri);
+    }
+  };
+
+  const onDismiss = () => setTakenPhoto('');
+
   return (
-    <View>
-      <Text>TakePhoto</Text>
-    </View>
+    <Container>
+      <StatusBar hidden={true} />
+      {takenPhoto === '' ? (
+        <Camera
+          ref={camera}
+          type={cameraType}
+          style={{ flex: 1 }}
+          zoom={zoom}
+          flashMode={flashMode}
+          onCameraReady={onCameraReady}
+        >
+          <CloseButton onPress={() => navigation.navigate('Tabs')}>
+            <Ionicons name="close" color="white" size={30} />
+          </CloseButton>
+        </Camera>
+      ) : (
+        <Image source={{ uri: takenPhoto }} styled={{ flex: 1 }} />
+      )}
+      {takenPhoto === '' ? (
+        <Actions>
+          <SliderContainer>
+            <Slider
+              style={{ width: 200, height: 40 }}
+              minimumValue={0}
+              maximumValue={1}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor="rgba(255,255,255,0.5)"
+              onValueChange={onZoomValueChange}
+            />
+          </SliderContainer>
+          <ButtonsContainer>
+            <TakePhotoBtn onPress={takePhoto} />
+            <ActionsContainer>
+              <TouchableOpacity onPress={onFlashChange} style={{ marginRight: 30 }}>
+                <Ionicons
+                  size={30}
+                  color="white"
+                  name={
+                    flashMode === Camera.Constants.FlashMode.off
+                      ? 'flash-off'
+                      : flashMode === Camera.Constants.FlashMode.on
+                      ? 'flash'
+                      : flashMode === Camera.Constants.FlashMode.auto
+                      ? 'eye'
+                      : ''
+                  }
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={onCameraSwitch}>
+                <Ionicons
+                  size={30}
+                  color="white"
+                  name={
+                    cameraType === Camera.Constants.Type.front
+                      ? 'camera-reverse'
+                      : 'camera'
+                  }
+                />
+              </TouchableOpacity>
+            </ActionsContainer>
+          </ButtonsContainer>
+        </Actions>
+      ) : (
+        <Actions>
+          <PhotoAction onPress={onDismiss}>
+            <PhotoActionText>Dismiss</PhotoActionText>
+          </PhotoAction>
+          <PhotoAction>
+            <PhotoActionText>Upload</PhotoActionText>
+          </PhotoAction>
+          <PhotoAction>
+            <PhotoActionText>Save & Upload</PhotoActionText>
+          </PhotoAction>
+        </Actions>
+      )}
+    </Container>
   );
 };
 
