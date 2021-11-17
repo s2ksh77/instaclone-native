21fa0ce421f551ad991f5f0cd0e4b39788357923
soohyun <soohyun_kim2@tmax.co.kr>
2021-11-17 13:39:31 +0900

    send message

--- screens/Room.js
+++ screens/Room.js
@@ -1,33 +1,12 @@
 import React, { useEffect } from 'react';
-import { FlatList, KeyboardAvoidingView } from 'react-native';
-import { gql, useQuery } from '@apollo/client';
+import { FlatList, KeyboardAvoidingView, View } from 'react-native';
+import { gql, useQuery, useMutation } from '@apollo/client';
 import ScreenLayout from '../components/ScreenLayout';
 import styled from 'styled-components/native';
-
-const SEND_MESSAGE_MUTATION = gql`
-  mutation sendMessage($payload: String!, $roomId: Int!, $userId: Int!) {
-    sendMessage(payload: $payload, roomId: $roomId, userId: $userId) {
-      ok
-      id
-    }
-  }
-`;
-
-const ROOM_QUERY = gql`
-  query seeRoom($id: Int!) {
-    seeRoom(id: $id) {
-      messages {
-        id
-        payload
-        user {
-          username
-          avatar
-        }
-        read
-      }
-    }
-  }
-`;
+import { useForm } from 'react-hook-form';
+import useMe from '../hooks/useMe';
+import client from '../apollo';
+import { Ionicons } from '@expo/vector-icons';
 
 const MessageContainer = styled.View`
   padding: 0px 10px;
@@ -52,27 +31,130 @@ const Message = styled.Text`
 `;
 
 const TextInput = styled.TextInput`
-  margin: 50px 0px 10px 0px;
-  width: 95%;
   border: 1px solid rgba(255, 255, 255, 0.5);
   padding: 10px 20px;
   border-radius: 1000px;
   color: white;
+  width: 90%;
+  margin-right: 10px;
+`;
+
+const InputContainer = styled.View`
+  margin: 25px 0px 10px 10px;
+  width: 95%;
+  flex-direction: row;
+  align-items: center;
+`;
+const SendButton = styled.TouchableOpacity``;
+
+const SEND_MESSAGE_MUTATION = gql`
+  mutation sendMessage($payload: String!, $roomId: Int, $userId: Int) {
+    sendMessage(payload: $payload, roomId: $roomId, userId: $userId) {
+      ok
+      id
+    }
+  }
+`;
+
+const ROOM_QUERY = gql`
+  query seeRoom($id: Int!) {
+    seeRoom(id: $id) {
+      messages {
+        id
+        payload
+        user {
+          username
+          avatar
+        }
+        read
+      }
+    }
+  }
 `;
 
 const Room = ({ route, navigation }) => {
-  const { data, loading } = useQuery(ROOM_QUERY, {
+  const { data, loading, refetch } = useQuery(ROOM_QUERY, {
     variables: {
       id: route?.params?.id,
     },
   });
-  console.log(data);
+  const { data: meData } = useMe();
+  const { register, handleSubmit, setValue, getValues, watch } = useForm();
+
+  // 방이 하나일 땐 cache가 Room:1 형태가 아닌 Room 이다.. 그래서 캐시가 잘 안먹음..
+  const updateSendMessage = async (cache, result) => {
+    const {
+      data: {
+        sendMessage: { ok, id },
+      },
+    } = result;
+    if (ok) {
+      // const { payload } = getValues();
+      // setValue('payload', '');
+      // const messageObj = {
+      //   __typename: 'Message',
+      //   id,
+      //   payload,
+      //   user: {
+      //     username: meData?.me?.username,
+      //     avatar: meData?.me?.avatar,
+      //   },
+      //   read: true,
+      // };
+      // const messageFragment = cache.writeFragment({
+      //   fragment: gql`
+      //     fragment NewMessage on Message {
+      //       id
+      //       payload
+      //       user {
+      //         username
+      //         avatar
+      //       }
+      //       read
+      //     }
+      //   `,
+      //   data: meesageObj,
+      // });
+      // cache.modify({
+      //   id: `Room:${route.params.id}`,
+      //   fields: {
+      //     messages(prev) {
+      //       return [...prev, messageFragment];
+      //     },
+      //   },
+      // });
+      setValue('payload', '');
+      await refetch();
+    }
+  };
+
+  const [sendMessageMutation, { loading: sendLoading }] = useMutation(
+    SEND_MESSAGE_MUTATION,
+    {
+      update: updateSendMessage,
+    }
+  );
 
   useEffect(() => {
     navigation.setOptions({
       title: `Conversation with ${route?.params?.talkingTo?.username}`,
     });
   }, []);
+
+  useEffect(() => {
+    register('payload', { required: true });
+  }, [register]);
+
+  const onValid = ({ payload }) => {
+    if (!sendLoading) {
+      sendMessageMutation({
+        variables: {
+          payload,
+          roomId: route?.params?.id,
+        },
+      });
+    }
+  };
 
   const renderItem = ({ item: message }) => (
     <MessageContainer
@@ -85,6 +167,8 @@ const Room = ({ route, navigation }) => {
     </MessageContainer>
   );
 
+  const messages = [...(data?.seeRoom?.messages ?? [])];
+  messages.reverse();
   return (
     <KeyboardAvoidingView
       style={{ flex: 1, backgroundColor: 'black' }}
@@ -93,18 +177,35 @@ const Room = ({ route, navigation }) => {
     >
       <ScreenLayout loading={loading}>
         <FlatList
-          style={{ width: '100%' }}
-          data={data?.seeRoom?.messages}
+          style={{ width: '100%', marginVertical: 10 }}
+          data={messages}
           keyExtractor={(message) => '' + message.id}
           renderItem={renderItem}
           inverted
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
         />
-        <TextInput
-          placeholder="Write a message..."
-          placeholderTextColor="rgba(255, 255, 255, 0.5)"
-          returnKeyLabel="전송"
-          returnKeyType="send"
-        />
+        <InputContainer>
+          <TextInput
+            placeholder="Write a message..."
+            placeholderTextColor="rgba(255, 255, 255, 0.5)"
+            returnKeyLabel="전송"
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit(onValid)}
+            onChangeText={(text) => setValue('payload', text)}
+            value={watch('payload')}
+          />
+          <SendButton
+            disabled={!Boolean(watch('payload'))}
+            onPress={handleSubmit(onValid)}
+          >
+            <Ionicons
+              name="send"
+              color={!Boolean(watch('payload')) ? 'rgba(255,255,255,0.5)' : 'white'}
+              size={22}
+            />
+          </SendButton>
+        </InputContainer>
       </ScreenLayout>
     </KeyboardAvoidingView>
   );
